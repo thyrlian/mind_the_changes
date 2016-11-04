1e63577f8c40e96455f9ce0dcc7261514c19820e
Jing Li <thyrlian@gmail.com>
2016-11-04 06:50:17 +0100

    Remove failing test

--- test/mind_the_changes_test.rb
+++ test/mind_the_changes_test.rb
@@ -4,8 +4,4 @@ class MindTheChangesTest < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::MindTheChanges::VERSION
   end
-
-  def test_it_does_something_useful
-    assert false
-  end
 end
